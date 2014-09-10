e3ba13a442743a39bac8461fcd60f6be24bfb6a3
maboughey <maboughey@gmail.com>
2014-09-10 13:10:22 +0100

    Fix all formatting

--- var/www/openmediavault/js/omv/module/admin/service/acestream/acestream.js
+++ var/www/openmediavault/js/omv/module/admin/service/acestream/acestream.js
@@ -1,7 +1,7 @@
 OMV.WorkspaceManager.registerNode({
-id: "acestream", // Individual ID
-path: "/service", // Parent folder in the navigation view
-text: _("AceStream Proxy"), // Text to show next to the icon
-icon16: "images/acestream.png", // 16x16 pixel icon that is displayed in the navigation view
-iconSvg: "images/acestream.svg" // SVG icon that is displayed in the navigation view
+    id:     "acestream", // Individual ID
+    path:   "/service", // Parent folder in the navigation view
+    text:   _("AceStream Proxy"), // Text to show next to the icon
+    icon16: "images/acestream.png", // 16x16 pixel icon that is displayed in the navigation view
+    iconSvg:"images/acestream.svg" // SVG icon that is displayed in the navigation view
 });
